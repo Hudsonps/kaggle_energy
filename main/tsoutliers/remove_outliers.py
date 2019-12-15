6ef081c806621bfa4f5728f2a1937caa959ee17f
Palermo Penano <palermo.penano@scotiabank.com>
2019-12-15 13:58:39 -0500

    drop and save

--- main/tsoutliers/remove_outliers.py
+++ main/tsoutliers/remove_outliers.py
@@ -1,4 +1,4 @@
-'''Eliminates and stitches together the output of R script
+'''Eliminates outliers and stitches together the output of R script
 that calculates the outlier score
 '''
 
@@ -7,23 +7,50 @@ import pandas as pd
 import feather
 from tqdm import tqdm
 
+
 if __name__ == '__main__':
-    TMP_PATH = pathlib.Path('/Users/palermopenano/personal/kaggle_energy/tmp')
+    MAIN = pathlib.Path('/Users/palermopenano/personal/kaggle_energy')
+    TMP_PATH = MAIN / 'tmp'
 
     files_list = list(TMP_PATH.glob('outlier_score*'))
-    main_df = feather.read_dataframe(files_list[0])
 
-    for o in tqdm(files_list[1:100]):
-    # for o in tqdm(files_list):
-        df = feather.read_dataframe(o)
-        main_df = pd.concat([df, main_df], ignore_index=True)
-        # print(
-        #     o.name,
-        #     main_df.shape,
-        #     sep='\n'
-        # )
+    print("Load all...")
+    dfs = [feather.read_dataframe(p) for p in tqdm(files_list)]
+    df_outliers = pd.concat(dfs, ignore_index=True)
 
-    feather.write_dataframe(main_df, TMP_PATH / 'train_nooutliers.csv')
+    # !!! Remove timezone information so we can merge it
+    # with the meter_readings dataset
+    df_outliers.timestamp = \
+        df_outliers.timestamp.dt.tz_localize(None)
 
-    print(main_df.head())
-    print(main_df.tail())
+    # Merge in meter readings
+    print("Loading and merging original meter reading data...")
+    df_train = pd.read_csv(MAIN / 'data' / 'train.csv')
+    df_train.timestamp = pd.to_datetime(
+        df_train.timestamp,
+        format="%Y-%m-%d %H:%M:%S"
+    )
+    df_train = df_train.merge(
+        df_outliers,
+        on=['building_id', 'meter', 'timestamp'],
+        how='left')
+
+    # Remove meter readings for which outlier
+    # score is greater than 0
+    num_out = len(df_train[df_train.outliers > 0.])
+    print(
+        "",
+        f"Number of outliers dropped: {num_out}",
+        f"Shape BEFORE dropping: {df_train.shape}",
+        sep='\n'
+    )
+    df_train = df_train[df_train.outliers == 0.]
+    print(f"Shape AFTER dropping: {df_train.shape}")
+    assert df_train.outliers.isna().sum() == 0
+
+    # Save result
+    df_train = df_train.drop('outliers', axis=1)
+    feather.write_dataframe(df_train, TMP_PATH / 'train_nooutliers.csv')
+
+    print(df_train.head())
+    print(df_train.tail())
